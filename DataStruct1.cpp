0978edf527db60373e0bdb280a3f693eab02dc91
Scott Weitze <swpro77@outlook.com>
2020-09-21 00:05:17 -0400

    Update DataStruct1.cpp

--- DataStruct1.cpp
+++ DataStruct1.cpp
@@ -1,8 +1,10 @@
 // DataStruct1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <cassert
+#include <cstdlib>
 #include <iostream>
 
+//GCD
 int gcd(int a, int b)
 {
 	if (b == 0)
@@ -12,8 +14,27 @@ int gcd(int a, int b)
 	return gcd(b, a % b);
 }
 
-int main()
+int lcm(int a, int b)
 {
+	return 0;
+}
+
+int powermod(int a, int b){
+	return 0;
+}
+
+/* 
+ *./exec a b r 
+ */
+int main(int argc, char** argv)
+{
+	assert(argc == 4); 
+	int a=0, b=0, r=0;
+	sscanf(argv[1],"%d",&a);
+	sscanf(argv[2],"%d",&b);
+	sscanf(argv[3],"%d",&r);
     std::cout << "Hello World!\n";
-	std::cout << gcd(92, 1001);
+	std::cout << "GCD: " << gcd(a, b) << std::endl;
+	std::cout << "LCM: " << lcm(a, b) << std::endl;
+	std::cout << "Power Mod: "<< powermod(a,b) << std::endl;
 }
